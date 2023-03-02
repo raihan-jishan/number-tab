2cdb12f578056f726d2bf2b377a60f10214c5cf8
Raihan <Raihan jishan@gmail.com>
2023-03-02 15:38:15 +0600

    first pushed

--- src/DATA/People.js
+++ src/DATA/People.js
@@ -1,86 +1,106 @@
 export const People = [
-    {
-        id:1, 
-        name: "আকবর ভাই , মেশ্রি ",
-        number:' 01711284691',
-        type:"undifined"
-    },
-    {
-        id:2, 
-        name:"আলেম মনিয়ন",
-        number:"01301401851",
-        type:"undifined" 
-    },
-    {
-        id:3, 
-        name:"ইমরান  কার  বাগমারা",
-        number:"018176185473",
-        type:"undifined" 
-    },
-    {
-        id:4, 
-        name:"আরব কাপড় দোকান , যুক্তিখুলা",
-        number:"01720974908",
-        type:"kapor dukan" 
-    },
-    {
-        id:5, 
-        name:"আজাদ হেলাল , লাইব্রেরি",
-        number:"01713866157",
-        type:"library" 
-    },
-    {
-        id:6, 
-        name:"দেলু,",
-        number:"01813274028",
-        type:"TO TO COMPANY" 
-    },
-    {
-        id:7, 
-        name:"দয়াল টিন, চট্টগ্রাম",
-        number:"01675522820",
-        type:"TIN কামলা" 
-    },
-    {
-        id:8, 
-        name:"দেলু তুলা, বিসমিল্লাহ",
-        number:"01851243919",
-        type:"টুলা বাবশাহ ",
-    },
-    {
-        id:8, 
-        name:"হাশেম পাকিস্তান",
-        number:"00923332269943",
-        type:"টো টো কাম্পানি" 
-    },
-    {
-        id:8, 
-        name:"haed master, cricent school",
-        number:"01816309550",
-        type:"master"
-    },
-    {
-        id:8, 
-        name:"বাবু, কালরা",
-        number:"01728438243",
-        type:"undifined!"
-    },
-    {
-        id:8, 
-        name:"বাহার কাকও, কমিশন ",
-        number:"01711575780",
-        type:"undifined!"
-    },
-    {
-        id:8, 
-        name:"হুজুর উত্তর বাজার ",
-        number:"01918648045",
-        type:"undifined!"
-    },
-    {
-        id:9,
-        name:"ব্রাক ব্যাংক, জাসিম ভাই, ",
-        number:"01847419332",
-        type:"undifined!"
-    }
+  {
+    id: 1,
+    name: "আজাদ হেলাল ",
+    number: " 01712866157",
+    icon: <i className="fa-solid fa-book"></i>,
+    type: " library",
+  },
+  {
+    id: 2,
+    name: "দেলু",
+    number: "01813274028",
+    icon: <i className="fas fa-search"></i>,
+    type: "TO TO COMPANY",
+  },
+  {
+    id: 3,
+    name: "দয়াল টিন, চট্টগ্রাম",
+    number: "01675522820",
+    icon: <i className="fas fa-search"></i>,
+    type: "TIN কামলা",
+  },
+  {
+    id: 4,
+    name: "দেলু তুলা, বিসমিল্লাহ",
+    number: "01851243919",
+    icon: <i className="fas fa-search"></i>,
+    type: "tula",
+  },
+  {
+    id: 5,
+    name: "হাশেম পাকিস্তান",
+    number: "00923332269943",
+    icon: <i className="fas fa-search"></i>,
+    type: "টো টো কাম্পানি",
+  },
+  {
+    id: 6,
+    name: "haed master, cricent school,",
+    number: "01816309550",
+    icon: <i className="fas fa-search"></i>,
+    type: "master",
+  },
+  {
+    id: 7,
+    name: "বাবু, কালরা",
+    number: "01728438243",
+    icon: <i className="fas fa-search"></i>,
+    type: "undifined",
+  },
+  {
+    id: 8,
+    name: "দেলু তুলা, বিসমিল্লাহ",
+    number: "01851243919",
+    icon: <i className="fas fa-search"></i>,
+    type: "টুলা বাবশাহ ",
+  },
+  {
+    id: 9,
+    name: "হাশেম পাকিস্তান",
+    number: "00923332269943",
+    icon: <i className="fas fa-search"></i>,
+    type: "টো টো কাম্পানি",
+  },
+  {
+    id: 10,
+    name: "haed master, cricent school",
+    number: "01816309550",
+    icon: <i className="fas fa-search"></i>,
+    type: "master",
+  },
+  {
+    id: 11,
+    name: "বাবু, কালরা",
+    number: "01728438243",
+    icon: <i className="fas fa-search"></i>,
+    type: "undifined!",
+  },
+  {
+    id: 12,
+    name: "বাহার কাকও, কমিশন ",
+    number: "01711575780",
+    icon: <i className="fas fa-search"></i>,
+    type: "undifined!",
+  },
+  {
+    id: 13,
+    name: "হুজুর উত্তর বাজার ",
+    number: "01918648045",
+    icon: <i className="fas fa-search"></i>,
+    type: "undifined!",
+  },
+  {
+    id: 14,
+    name: "ব্রাক ব্যাংক, জাসিম ভাই, ",
+    number: "01847419332",
+    icon: <i className="fas fa-search"></i>,
+    type: "undifined!",
+  },
+  {
+    id:15, 
+    name:"হিরন পুলিশ",
+    number:"01991127531",
+    icon:<i className="fas fa-police"></i>,
+  }
 ];
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,32 +1,25 @@
-import React from 'react';
+import React from "react";
 import "../css/home.css";
-import {People} from "../DATA/People";
+import { People } from "../DATA/People";
 const Home = () => {
   return (
-    <div className='container'>
-        <div className="title">
-            <h1>People Number Tab </h1>
-        </div>
-        
-        {People.map((user) => {
-          return (
-        
-              <div className='dataTable'>
+    <div className="container">
+      <div className="title">
+        <h1>People Number Tab </h1>
+      </div>
+
+      {People.map((user) => {
+        return (
+          <div className="dataTable">
             <h3>Name: {user.name}</h3>
             <h3>Number:{user.number}</h3>
-            <h3>Type:{user.type}</h3>
+            <h3 className="type">Type:{user.type} <span className="icon">{user.icon}</span></h3>
             
           </div>
-          )
-          } )}
+        );
+      })}
+    </div>
+  );
+};
 
-          
-          
-      </div>
-          
- 
- 
-  )
-}
-
-export default Home
+export default Home;
