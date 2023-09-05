2f733ced02c8d6c7353fb6d9da5cc32a68484a89
raihan-jishan <112199871+raihan-jishan@users.noreply.github.com>
2023-09-05 15:54:02 +0600

    Update OldnumberList.jsx

--- src/components/OldnumberList/OldnumberList.jsx
+++ src/components/OldnumberList/OldnumberList.jsx
@@ -5,7 +5,7 @@
 */
 // import all important files and modules
 import React from "react"; 
-import { AiOutlineUser, AiOutlineHeart, AiOutlineTag, AiOutlineHeart } from "react-icons/ai";
+import { AiOutlineUser, AiOutlineHeart, AiOutlineTag  } from "react-icons/ai";
 import { FaShare } from "react-icons/fa";
 import "../../css/numbers.css";
 import { SkeletonTheme } from "react-loading-skeleton";
