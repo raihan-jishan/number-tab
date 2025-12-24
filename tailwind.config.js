f141e07ff7507b953b735732c951e71772f72420
raihan-jishan <raihanjishan3242@gmail.com>
2025-12-25 00:35:18 +0600

    update the add contact page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: false,
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+
   theme: {
     fontFamily: {
       Comfortaa: ["Comfortaa"],
@@ -10,51 +10,63 @@ export default {
       Manrope: ["Manrope", "sans-serif"],
       Dosis: ["Dosis", "sans-serif"],
       Lato: ["Lato", "sans-serif"],
-      Inter: ["Inter", "sans-serif"]
+      Inter: ["Inter", "sans-serif"],
     },
+
     extend: {
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
+
       colors: {
         Primary: "#181A1B",
         Secondary: "#22c55e",
         Tertiary: "#f3f4f6",
+
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
+
         card: {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
+
         popover: {
           DEFAULT: "hsl(var(--popover))",
           foreground: "hsl(var(--popover-foreground))",
         },
+
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
         },
+
         secondary: {
           DEFAULT: "hsl(var(--secondary))",
           foreground: "hsl(var(--secondary-foreground))",
         },
+
         muted: {
           DEFAULT: "hsl(var(--muted))",
           foreground: "hsl(var(--muted-foreground))",
         },
+
         accent: {
           DEFAULT: "hsl(var(--accent))",
           foreground: "hsl(var(--accent-foreground))",
         },
+
         destructive: {
           DEFAULT: "hsl(var(--destructive))",
           foreground: "hsl(var(--destructive-foreground))",
         },
+
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
+
         chart: {
           1: "hsl(var(--chart-1))",
           2: "hsl(var(--chart-2))",
@@ -63,16 +75,19 @@ export default {
           5: "hsl(var(--chart-5))",
         },
       },
-    },
-    animation: {
-      "fade-in": "fadeIn 0.2s ease-in-out",
-    },
-    keyframes: {
-      fadeIn: {
-        "0%": { opacity: 0, transform: "translateY(-4px)" },
-        "100%": { opacity: 1, transform: "translateY(0)" },
+
+      animation: {
+        "fade-in": "fadeIn 0.2s ease-in-out",
+      },
+
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0, transform: "translateY(-4px)" },
+          "100%": { opacity: 1, transform: "translateY(0)" },
+        },
       },
     },
   },
+
   plugins: [require("tailwindcss-animate")],
 };
